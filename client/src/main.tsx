aeefd92e24b109f4f4c29f1c832ca5e1a518d159
Romain Martinez <romain.martinezquey@MacBook-Pro-2.local>
2024-06-21 16:28:28 +0200

    feat(src/App.tsx): login in progress with shadcn

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import { ApolloProvider } from '@apollo/client';
-import client from './apollo-client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo-client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <Router>
