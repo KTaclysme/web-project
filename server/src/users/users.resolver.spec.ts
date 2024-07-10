7447b0ce5abfd63d805267d7e4698736523aa6ae
Val9c <62969329+Val9c@users.noreply.github.com>
2024-07-10 15:07:53 +0200

    Creation des tests(auth, user, bullmq) à verfiier svp :)

--- server/src/users/users.resolver.spec.ts
+++ server/src/users/users.resolver.spec.ts
@@ -1,18 +1,93 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+import { GqlJwtAuthGuard } from '../auth/gql-jwt-auth.guard';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
+  let userService: UsersService;
 
+  // Avant chaque test, configurer le module de test
   beforeEach(async () => {
+    // Créer un module de test avec UsersResolver et un UsersService mocké
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
-    }).compile();
+      providers: [
+        UsersResolver,
+        {
+          provide: UsersService,
+          useValue: {
+            findOneById: jest.fn(),  // Mock de la méthode findOneById du UsersService
+            findAll: jest.fn(),      // Mock de la méthode findAll du UsersService
+          },
+        },
+      ],
+    })
+      .overrideGuard(GqlJwtAuthGuard)  // Remplacer le GqlJwtAuthGuard par une version mockée
+      .useValue({ canActivate: () => true })  // Définir le comportement du mock du GqlJwtAuthGuard
+      .compile();
 
-    resolver = module.get<UsersResolver>(UsersResolver);
+    resolver = module.get<UsersResolver>(UsersResolver);  // Récupérer l'instance du UsersResolver
+    userService = module.get<UsersService>(UsersService);  // Récupérer l'instance mockée du UsersService
   });
 
+  // Test pour vérifier que le resolver est défini
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  // Bloc de tests pour la méthode me
+  describe('me', () => {
+    it('should return the current user', async () => {
+      const mockUserId = 1;
+      const mockUser = { id: mockUserId, username: 'testuser', password: 'hashedpassword' };
+
+      const mockContext = {
+        req: {
+          user: { userId: mockUserId },  // Contexte simulé avec l'ID utilisateur
+        },
+      };
+
+      jest.spyOn(userService, 'findOneById').mockResolvedValue(mockUser);  // Espionner findOneById pour retourner mockUser
+
+      const result = await resolver.me(mockContext);  // Appeler la méthode me du resolver avec le mockContext
+
+      // Vérifier que le résultat correspond au mockUser et que findOneById a été appelé avec mockUserId
+      expect(result).toBe(mockUser);
+      expect(userService.findOneById).toHaveBeenCalledWith(mockUserId);
+    });
+  });
+
+  // Bloc de tests pour la méthode users
+  describe('users', () => {
+    it('should return an array of users', async () => {
+      const mockUsers = [
+        { id: 1, username: 'testuser1', password: 'hashedpassword1' },
+        { id: 2, username: 'testuser2', password: 'hashedpassword2' },
+      ];
+
+      jest.spyOn(userService, 'findAll').mockResolvedValue(mockUsers);  // Espionner findAll pour retourner mockUsers
+
+      const result = await resolver.users();  // Appeler la méthode users du resolver
+
+      // Vérifier que le résultat correspond à mockUsers et que findAll a été appelé
+      expect(result).toBe(mockUsers);
+      expect(userService.findAll).toHaveBeenCalled();
+    });
+  });
+
+  // Bloc de tests pour la méthode user
+  describe('user', () => {
+    it('should return a user by id', async () => {
+      const userId = 1;
+      const mockUser = { id: userId, username: 'testuser', password: 'hashedpassword' };
+
+      jest.spyOn(userService, 'findOneById').mockResolvedValue(mockUser);  // Espionner findOneById pour retourner mockUser
+
+      const result = await resolver.user(userId);  // Appeler la méthode user du resolver avec userId
+
+      // Vérifier que le résultat correspond à mockUser et que findOneById a été appelé avec userId
+      expect(result).toBe(mockUser);
+      expect(userService.findOneById).toHaveBeenCalledWith(userId);
+    });
   });
 });
--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -1,18 +1,132 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let prismaService: PrismaService;
 
+  // Avant chaque test, configurer le module de test
   beforeEach(async () => {
+    // Créer un module de test avec UsersService et un PrismaService mocké
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              create: jest.fn(),    // Mock de la méthode create du service Prisma pour l'entité 'user'
+              findMany: jest.fn(),  // Mock de la méthode findMany du service Prisma pour l'entité 'user'
+              findUnique: jest.fn(),// Mock de la méthode findUnique du service Prisma pour l'entité 'user'
+            },
+          },
+        },
+      ],
     }).compile();
 
+    // Récupérer les instances du UsersService et du PrismaService
     service = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
+  // Test pour vérifier que le UsersService est correctement défini
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // Bloc de tests pour la méthode create
+  describe('create', () => {
+    it('should create a new user with hashed password', async () => {
+      const data = { username: 'testuser', password: 'testpass' };
+      const hashedPassword = 'hashedPassword'; // Valeur de hachage fixe pour les tests
+      const user = { id: 1, username: data.username, password: hashedPassword };
+
+      // Espionner la fonction hash de bcrypt pour retourner une valeur de hachage fixe
+      jest.spyOn(bcrypt, 'hash').mockResolvedValue(hashedPassword);
+      // Espionner la méthode create du PrismaService pour retourner l'utilisateur créé
+      jest.spyOn(prismaService.user, 'create').mockResolvedValue(user);
+
+      // Appeler la méthode create du UsersService et vérifier le résultat
+      expect(await service.create(data)).toEqual(user);
+      // Vérifier que la méthode create du PrismaService a été appelée avec les bonnes données
+      expect(prismaService.user.create).toHaveBeenCalledWith({
+        data: {
+          username: data.username,
+          password: hashedPassword,
+        },
+      });
+    });
+  });
+
+  // Bloc de tests pour la méthode findAll
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const users = [
+        { id: 1, username: 'testuser1', password: 'testpass1' },
+        { id: 2, username: 'testuser2', password: 'testpass2' },
+      ];
+
+      // Espionner la méthode findMany du PrismaService pour retourner un tableau d'utilisateurs
+      jest.spyOn(prismaService.user, 'findMany').mockResolvedValue(users);
+
+      // Appeler la méthode findAll du UsersService et vérifier le résultat
+      expect(await service.findAll()).toEqual(users);
+      // Vérifier que la méthode findMany du PrismaService a été appelée
+      expect(prismaService.user.findMany).toHaveBeenCalled();
+    });
+  });
+
+  // Bloc de tests pour la méthode findOneById
+  describe('findOneById', () => {
+    it('should return a user by id', async () => {
+      const userId = 1;
+      const user = { id: userId, username: 'testuser', password: 'testpass' };
+
+      // Espionner la méthode findUnique du PrismaService pour retourner un utilisateur
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(user);
+
+      // Appeler la méthode findOneById du UsersService avec userId et vérifier le résultat
+      expect(await service.findOneById(userId)).toEqual(user);
+      // Vérifier que la méthode findUnique du PrismaService a été appelée avec les bons paramètres
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({ where: { id: userId } });
+    });
+
+    it('should return null if user is not found', async () => {
+      // Simuler que findUnique du PrismaService retourne null
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(null);
+
+      // Appeler la méthode findOneById du UsersService avec un ID inexistant et vérifier le résultat
+      expect(await service.findOneById(99)).toBeNull();
+      // Vérifier que la méthode findUnique du PrismaService a été appelée avec les bons paramètres
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({ where: { id: 99 } });
+    });
+  });
+
+  // Bloc de tests pour la méthode findOneByUsername
+  describe('findOneByUsername', () => {
+    it('should return a user by username', async () => {
+      const username = 'testuser';
+      const user = { id: 1, username: username, password: 'testpass' };
+
+      // Espionner la méthode findUnique du PrismaService pour retourner un utilisateur
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(user);
+
+      // Appeler la méthode findOneByUsername du UsersService avec username et vérifier le résultat
+      expect(await service.findOneByUsername(username)).toEqual(user);
+      // Vérifier que la méthode findUnique du PrismaService a été appelée avec les bons paramètres
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({ where: { username: username } });
+    });
+
+    it('should return null if user is not found', async () => {
+      // Simuler que findUnique du PrismaService retourne null
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(null);
+
+      // Appeler la méthode findOneByUsername du UsersService avec un nom d'utilisateur inexistant et vérifier le résultat
+      expect(await service.findOneByUsername('unknownuser')).toBeNull();
+      // Vérifier que la méthode findUnique du PrismaService a été appelée avec les bons paramètres
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({ where: { username: 'unknownuser' } });
+    });
   });
 });
